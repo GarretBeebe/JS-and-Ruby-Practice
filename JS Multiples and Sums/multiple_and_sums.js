b985058522f0b44ca46554092243b5671d86a89b
Garret Beebe <garret.beebe@gmail.com>
2016-12-09 15:19:52 -0800

    added argv for changing range of sum

--- JS Multiples and Sums/multiple_and_sums.js
+++ JS Multiples and Sums/multiple_and_sums.js
@@ -1,7 +1,11 @@
 'use strict';
 var _ = require('underscore');
-var range = _.range(0,10, 1);
 var multiples = [];
+var range = [];
+var rangeEnd;
+
+rangeEnd = process.argv.slice(2);
+range = _.range(0,rangeEnd, 1);
 
 range.forEach(function(number){
 	if ((number % 3 == 0) || (number % 5 == 0)) {
@@ -10,4 +14,4 @@ range.forEach(function(number){
 })
 
 var sum = _.reduce(multiples, function(memo, num){ return memo + num; }, 0);
-console.log(sum);
+console.log('The sum of multiples of 3 and 5 between 0 and ' + rangeEnd + ' is ' +sum);
