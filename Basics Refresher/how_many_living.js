308fa2a742023f3448fdd14cda3485ae8f770702
Garret Beebe <garret@TK-421-MAC.local>
2019-08-20 09:43:42 -0700

    cleaning up creating some functions

--- Basics Refresher/how_many_living.js
+++ Basics Refresher/how_many_living.js
@@ -1,4 +1,3 @@
-var _ = require('underscore');
 lifeSpans = [
   [1928, 1973],
   [1860, 1910],
@@ -6,37 +5,40 @@ lifeSpans = [
   [1935, 2000]
 ]
 
-histogram = {};
-
-for (i = 0; i < lifeSpans.length; i++) {
-  for (j = lifeSpans[i][0]; j< lifeSpans[i][1]; j++) {
-    if (j in histogram) {
-      histogram[j] = histogram[j] + 1;
+function createHistogram(lifeSpans) {
+  let histogram = {};
+  for (i = 0; i < lifeSpans.length; i++) {
+    for (j = lifeSpans[i][0]; j< lifeSpans[i][1]; j++) {
+      if (j in histogram) {
+        histogram[j] = histogram[j] + 1;
+      }
+      else {
+        histogram[j] = 1;
+      }
     }
-    else {
-      histogram[j] = 1;
+  }
+  return histogram;
+}
+
+function findMaxYears(histogram){
+  let maxYears = [];
+  let finalMax = 0
+
+  for (key in histogram) {
+    if (histogram[key] >= finalMax) {
+        finalMax = histogram[key]
     }
   }
-}
 
-console.log(histogram);
-
-maxYears = [];
-currentMax = 0
-finalMax = 0
-
-for (key in histogram) {
-  if (histogram[key] >= finalMax) {
-      finalMax = histogram[key]
+  for (key in histogram) {
+    if (histogram[key] == finalMax) {
+        maxYears.push(key);
+    }
   }
-}
-console.log(finalMax);
-
-for (key in histogram) {
-  if (histogram[key] == finalMax) {
-      maxYears.push(key);
-  }
+  return maxYears;
 }
 
+var histogram = createHistogram(lifeSpans);
+var maxYears = findMaxYears(histogram);
 console.log(maxYears);
 
